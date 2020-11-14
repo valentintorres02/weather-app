fad6d38424ec3a2dc9424846f62b2f679083c554
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2020-11-14 19:30:33 -0300

    Create Weather.js

---
+++ src/Components/Weather.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Weather(props) {
+    return (
+        <div>
+            HOLA
+    <p>{props.weather.temperatura}</p>
+        </div>
+    )
+}
+
+export default Weather
