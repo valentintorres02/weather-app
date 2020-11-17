0f17d0f210499f00b1e81dc96bee772579303ae6
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2020-11-16 22:38:34 -0300

    Update Search.js
    Adds eslint

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,45 +1,57 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-function Search(props) {
+function Search({ date }) {
+  Search.propTypes = {
+    date: PropTypes.func.isRequired,
+  };
 
   const [search, setSearch] = useState('');
-  const [data, setData] = useState({})
+  const [data, setData] = useState({});
 
-  const API_WEB = 'http://api.openweathermap.org/data/2.5/'
-  const API_KEY = '90edfdd804711bd4864a9a0f55293bff'
-
+  const API_WEB = 'http://api.openweathermap.org/data/2.5/';
+  const API_KEY = '90edfdd804711bd4864a9a0f55293bff';
 
   const fetching = () => {
-
     fetch(`${API_WEB}weather?q=${search}&appid=${API_KEY}`)
-    .then(r => r.json())
-    .then(r => {setData(r); setSearch(''); console.log(r)})
-  }
-  
-  const kelvinToCelsius = (degrees) => {
-    return degrees - 273.1
-  }
-    return (
+      .then((r) => r.json())
+      .then((r) => { setData(r); setSearch(''); });
+  };
+
+  const kelvinToCelsius = (degrees) => degrees - 273.1;
+
+  return (
+    <div>
+      <div>
+        <form className="form-inline search-box" onSubmit={(e) => e.preventDefault()}>
+          <div>
+
+            <input className="form-control" type="text" placeholder="Enter city name" onChange={(e) => setSearch(e.target.value)} value={search} />
+            <button className="btn btn-primary" onClick={() => fetching()} type="button">Search</button>
+          </div>
+        </form>
+      </div>
+      {(typeof data.main !== 'undefined') ? (
+
         <div>
-            <div>
-                <form className="form-inline search-box" onSubmit={e => e.preventDefault()}>
-                  <div>
-
-                <input className="form-control" type="text" placeholder="Enter city name" onChange={e => setSearch(e.target.value)}value={search}></input>
-                <button className="btn btn-primary" onClick = {() => fetching()}type="button">Search</button>
-                  </div>
-                </form>
-            </div>
-            {(typeof data.main != 'undefined') ? (
-
-              <div>
-    <p>{data.name}, {data.sys.country}</p>
-    <p>{kelvinToCelsius(data.main.temp).toFixed(2)}°</p>
-            <p>{data.weather[0].main}</p>
-    <p>{props.weatherData.date}</p>
-            </div>
-        ) : ('')}
+          <p>
+            {data.name}
+            ,
+            {' '}
+            {data.sys.country}
+          </p>
+          <p>
+            {kelvinToCelsius(data.main.temp).toFixed(2)}
+            °
+          </p>
+          <p>{data.weather[0].main}</p>
+          <p>
+            {date}
+          </p>
         </div>
-    )}
+      ) : ('')}
+    </div>
+  );
+}
 
-export default Search
+export default Search;
