40c4e569894db55c6dc56001763f81f6dd1adbd6
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2020-11-16 22:38:37 -0300

    Update App.js
    Adds eslint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import './App.css';
-import Search from './Components/Search'
+import Search from './Components/Search';
 
 function App() {
-
   const dateBuilder = (d) => {
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
+    const day = days[d.getDay()];
+    const date = d.getDate();
+    const month = months[d.getMonth()];
+    const year = d.getFullYear();
 
-    return `${day} ${date} ${month} ${year}`
-  }
+    return `${day} ${date} ${month} ${year}`;
+  };
   return (
     <div>
-        <h1 className="title">WEATHER</h1>
+      <h1 className="title">WEATHER</h1>
       <div className="search-data">
-      <Search weatherData={{date: dateBuilder(new Date())}}/>
+        <Search date={dateBuilder(new Date())} />
       </div>
     </div>
   );
