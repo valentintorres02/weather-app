002c612fd1828fdff52b8b11031dc77083274bf5
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-05-04 16:51:03 -0300

    Fix: Modularize App and Title into separate files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
 import './App.css';
-import Search from './Components/Search';
+import Search from './components/Search/Search';
+import Title from './components/Title/Title';
+import { dateBuilder } from './constants/constants';
 
 function App() {
-  const dateBuilder = (d) => {
-    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
-    const day = days[d.getDay()];
-    const date = d.getDate();
-    const month = months[d.getMonth()];
-    const year = d.getFullYear();
-
-    return `${day} ${date} ${month} ${year}`;
-  };
   return (
-    <div>
-      <h1 className="title">WEATHER</h1>
-      <div className="search-data">
-        <Search date={dateBuilder(new Date())} />
-      </div>
-    </div>
+    <>
+      <Title />
+      <Search date={dateBuilder(new Date())} />
+    </>
   );
 }
 
---
+++ src/Components/Title/Title.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../App.css';
+
+function Title() {
+  return (
+    <h1 className="title">WEATHER</h1>
+  );
+}
+
+export default Title;
