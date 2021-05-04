205b43c109f483a45ea77cabb6001961d91ea004
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-05-04 17:46:23 -0300

    Fix: Rename Search component

--- src/Components/Search/SearchComponent.js
+++ src/Components/Search/SearchComponent.js
@@ -3,47 +3,30 @@ import PropTypes from 'prop-types';
 import '../../App.css';
 import { API_WEB, API_KEY } from '../../constants/constants';
 import SearchInput from './SearchInput';
+import WeatherInfo from '../Weather.js/WeatherInfo';
 
 function Search({ date }) {
-  Search.propTypes = {
-    date: PropTypes.func.isRequired,
-  };
-
   const [search, setSearch] = useState('');
-  const [data, setData] = useState({});
+  const [weatherData, setWeatherData] = useState({});
 
   const fetching = () => {
     fetch(`${API_WEB}weather?q=${search}&appid=${API_KEY}`)
       .then((r) => r.json())
-      .then((r) => { setData(r); setSearch(''); });
+      .then((r) => { setWeatherData(r); setSearch(''); });
   };
-
-  const kelvinToCelsius = (degrees) => degrees - 273.1;
 
   return (
     <div className="search-data">
       <SearchInput search={search} setSearch={setSearch} fetching={fetching} />
-      {(typeof data.main !== 'undefined') ? (
-
-        <div>
-          <p>
-            {data.name}
-            ,
-            {' '}
-            {data.sys.country}
-          </p>
-          <p>
-            {kelvinToCelsius(data.main.temp).toFixed(2)}
-            °
-          </p>
-          <p>{data.weather[0].main}</p>
-          <p>
-            {date}
-          </p>
-        </div>
+      {(typeof weatherData.main !== 'undefined') ? (
+        <WeatherInfo weatherData={weatherData} date={date} />
       ) : ('')}
     </div>
   );
 }
+
+Search.propTypes = {
+  date: PropTypes.func.isRequired,
+};
 
 export default Search;
