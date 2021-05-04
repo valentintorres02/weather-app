2885933b6d6b76fdd39d467c4c33365e046975b1
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-05-04 16:48:59 -0300

    Feature: Create dedicated constants folder

---
+++ src/constants/constants.js
@@ -0,0 +1,16 @@
+export const months = [
+  'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December',
+];
+
+export const days = [
+  'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday',
+];
+
+export const dateBuilder = (d) => {
+  const day = days[d.getDay()];
+  const date = d.getDate();
+  const month = months[d.getMonth()];
+  const year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`;
+};
